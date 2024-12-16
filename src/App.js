030ceac6e3eff3f1875e847c3161de9aa9964e73
Batchuluun0605 <batchuluun655@gmail.com>
2024-12-16 10:34:10 +0800

    add component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Content from "./components/Content.jsx";
+
 function App() {
-  return <div>date filter to react</div>;
+  return (
+    <div>
+      date filter to react
+      <div>
+        <Content />
+      </div>
+    </div>
+  );
 }
 
 export default App;
